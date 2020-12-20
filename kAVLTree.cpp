86dcd30380af1ea5415d539d5fa493dae6eeb449
Nico Wong <nicowong8@gmail.com>
2020-12-19 20:15:35 -0800

    fixed Node constructor so that it initializes left and right ptrs

--- kAVLTree.cpp
+++ kAVLTree.cpp
@@ -221,7 +221,7 @@ kAVLTree::Node* kAVLTree::createNode(int whole, int frac) const
 /** START: Node member functions **/
 
 // Parameterized Constructor
-kAVLTree::Node::Node(int whole, int fract) : height(0), value(whole, fract)
+kAVLTree::Node::Node(int whole, int fract) : height(0), value(whole, fract), left(nullptr), right(nullptr)
 {}
 
 /** END: Node member functions **/
